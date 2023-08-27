58052b8bdca46c8614b9ee1122ff40a82e06a370
rozari0 <rozario@tuta.io>
2023-08-27 15:07:52 +0600

    Bleh

--- LearnVanilaJS/GuessingGame/script.js
+++ LearnVanilaJS/GuessingGame/script.js
@@ -1,5 +1,5 @@
 document.querySelector('form').addEventListener("submit", Check);
-
+document.querySelector('input').addEventListener("keyup",CheckIfOneDigit);
 // From MDN
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -10,6 +10,13 @@ randomNum = getRandomInt(1, 9 + 1)
 console.log(randomNum)
 chance = 3;
 
+function CheckIfOneDigit(e){
+  guess = document.querySelector('#guess').value;
+  if (guess.length > 1){
+    document.querySelector('#guess').value = guess[0];
+  }
+}
+
 function Check(e) {
   e.preventDefault();
   guess = document.querySelector('#guess').value
@@ -17,7 +24,17 @@ function Check(e) {
     return alert("Please Enter A Number.")
   }
   if (guess == randomNum) {
-    return document.write("You Win!")
+    document.body.innerHTML = ""
+    header = document.createElement('h1');
+    header.innerHTML = "You Win!";
+    document.body.appendChild(header);
+    button = document.createElement('button');
+    button.innerHTML = "Play Again?"
+    button.onclick = function () {
+      location.reload();
+    }
+    document.body.appendChild(button);
+    return
   }
   else if (guess < randomNum) {
     document.querySelector('p').innerHTML = "Secret Number is bigger than your guess.";
@@ -28,7 +45,17 @@ function Check(e) {
     chance -= 1
   }
   if (chance < 1) {
-    return document.write("You Lose.")
+    document.body.innerHTML = ""
+    header = document.createElement('h1');
+    header.innerHTML = "You Lose!";
+    document.body.appendChild(header);
+    button = document.createElement('button');
+    button.innerHTML = "Try Again?"
+    button.onclick = function () {
+      location.reload();
+    }
+    document.body.appendChild(button);
+    return
   }
 
 }
